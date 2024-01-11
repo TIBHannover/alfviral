1ebfb73a07d0e12f5fa3baaf995235af7718d645
Edmond Kacaj <programim95@gmail.com>
2024-01-11 08:43:18 +0100

    fix: inport InputStream

--- alfviral/src/main/java/com/fegor/alfresco/security/antivirus/InStreamScan.java
+++ alfviral/src/main/java/com/fegor/alfresco/security/antivirus/InStreamScan.java
@@ -21,6 +21,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
+import java.io.InputStream;
 import java.net.Socket;
 import java.net.SocketException;
 
