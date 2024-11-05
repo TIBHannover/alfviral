9867cb624a079c471f36d147c11ec886ec6e2a17
Edmond Kacaj <edmondikacaj@gmail.com>
2024-11-05 09:03:09 +0100

    fix(alfviral-platform): Use Stream instead of bytes, for larger files

--- alfviral-platform/src/main/java/com/fegorsoft/alfresco/security/antivirus/InStreamScan.java
+++ alfviral-platform/src/main/java/com/fegorsoft/alfresco/security/antivirus/InStreamScan.java
@@ -16,13 +16,11 @@
  */
 package com.fegorsoft.alfresco.security.antivirus;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
 
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -32,255 +30,254 @@ import com.fegorsoft.alfresco.model.AlfviralModel;
 
 /**
  * InStreamScan
- * 
- * @author fegor
  *
+ * @author fegor
  */
 public final class InStreamScan implements VirusScanMode {
 
-	private final Logger logger = Logger.getLogger(InStreamScan.class);
+    private final Logger logger = Logger.getLogger(InStreamScan.class);
 
-	private byte[] data;
-	private int chunkSize = 4096;
-	private int port;
-	private String host;
-	private int timeout;
-	private NodeService nodeService;
-	private NodeRef nodeRef;
+    private InputStream data;
+    private int chunkSize = 4096;
+    private int port;
+    private String host;
+    private int timeout;
+    private NodeService nodeService;
+    private NodeRef nodeRef;
 
-	/**
-	 * Constructor
-	 */
-	public InStreamScan() {
-	}
+    /**
+     * Constructor
+     */
+    public InStreamScan() {
+    }
 
-	/**
-	 * Test connection
-	 * 
-	 * @return test of connection
-	 */
-	public boolean testConnection() {
-		boolean result = true;
-		
-		logger.info(getClass().getName() + "Testing connect to " + host.toString() + ":" + port);
-		Socket socket = new Socket();
-		try {
-			socket.connect(new InetSocketAddress(host, port));
-		} catch (IOException ioe) {
-			logger.error(getClass().getName() + "Error connecting to " + host.toString() + ":" + port);
-			ioe.printStackTrace();
-			result = false;
-		} finally {
-			if (socket.isConnected()) {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					logger.error(getClass().getName() + "Error closing to " + host.toString() + ":" + port);
-					e.printStackTrace();
-					result = false;
-				}
-			}
-		}
-		
-		if (result == true) {
-			logger.info(getClass().getName() + "Connect to INSTREAM is OK");
-		}
-		
-		return result;
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.fegorsoft.alfresco.security.antivirus.VirusScanMode#scan(org.alfresco.service.cmr.repository.NodeRef)
-	 */
-	@Override
-	public int scan(NodeRef nodeRef) {
-		int res = 0;
-		this.nodeRef = nodeRef;
-		try {
-			res = scan();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return res;
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.fegorsoft.alfresco.security.antivirus.VirusScanMode#scan()
-	 */
-	@Override
-	public int scan() throws IOException {
-		int i = 0;
-		int result = 0;
+    /**
+     * Test connection
+     *
+     * @return test of connection
+     */
+    @SuppressWarnings("unused")
+    public boolean testConnection() {
+        boolean result = true;
 
-		/*
-		 * create socket
-		 */
-		if (logger.isDebugEnabled()) {
-			logger.debug(getClass().getName() + "Connect to " + host + ":" + port);
-		}
-		
-		Socket socket = new Socket();
-		socket.connect(new InetSocketAddress(host, port));
+        logger.info(getClass().getName() + "Testing connect to " + host + ":" + port);
+        Socket socket = new Socket();
+        try {
+            socket.connect(new InetSocketAddress(host, port));
+        } catch (IOException ioe) {
+            logger.error(getClass().getName() + "Error connecting to " + host + ":" + port);
+            ioe.printStackTrace();
+            result = false;
+        } finally {
+            if (socket.isConnected()) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    logger.error(getClass().getName() + "Error closing to " + host + ":" + port);
+                    e.printStackTrace();
+                    result = false;
+                }
+            }
+        }
 
-		try {
-			socket.setSoTimeout(timeout);
-		} catch (SocketException e) {
-			logger.error("Error in timeout: " + timeout + "ms", e);
-		}
+        if (result) {
+            logger.info(getClass().getName() + "Connect to INSTREAM is OK");
+        }
 
-		DataOutputStream dataOutputStream = null;
-		BufferedReader bufferedReader = null;
+        return result;
+    }
 
-		String res = null;
-		try {
-			if (logger.isDebugEnabled()) {
-				logger.debug(getClass().getName() + "Send zINSTREAM");
-			}
-			
-			dataOutputStream = new DataOutputStream(socket.getOutputStream());
-			dataOutputStream.writeBytes("zINSTREAM\0");
+    /* (non-Javadoc)
+     * @see com.fegorsoft.alfresco.security.antivirus.VirusScanMode#scan(org.alfresco.service.cmr.repository.NodeRef)
+     */
+    @Override
+    public int scan(NodeRef nodeRef) {
+        int res = 0;
+        this.nodeRef = nodeRef;
+        try {
+            res = scan();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
 
-			if (logger.isDebugEnabled()) {
-				logger.debug(getClass().getName() + "Send stream for  " + data.length + " bytes");
-			}
+    /*
+     * (non-Javadoc)
+     *
+     * @see com.fegorsoft.alfresco.security.antivirus.VirusScanMode#scan()
+     */
+    @Override
+    public int scan() throws IOException {
+        int bytesRead;
+        int result = 0;
+        byte[] buffer = new byte[chunkSize];
+        /*
+         * create socket
+         */
+        if (logger.isDebugEnabled()) {
+            logger.debug(getClass().getName() + "Connect to " + host + ":" + port);
+        }
 
-			while (i < data.length) {
-				if (i + chunkSize >= data.length) {
-					chunkSize = data.length - i;
-				}
-				dataOutputStream.writeInt(chunkSize);
-				dataOutputStream.write(data, i, chunkSize);
-				i += chunkSize;
-			}
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress(host, port));
 
-			dataOutputStream.writeInt(0);
-			dataOutputStream.write('\0');
-			dataOutputStream.flush();
+        try {
+            socket.setSoTimeout(timeout);
+        } catch (SocketException e) {
+            logger.error("Error in timeout: " + timeout + "ms", e);
+        }
 
-			bufferedReader = new BufferedReader(new InputStreamReader(
-					socket.getInputStream(), "ASCII"));
-				
-			res = bufferedReader.readLine();
-			
-			if (logger.isDebugEnabled()) {
-				logger.debug(getClass().getName() + "Result of scan is:  " + res);
-			}
-			
-		} finally {
-			if (bufferedReader != null)
-				bufferedReader.close();
-			
-			if (dataOutputStream != null)
-				dataOutputStream.close();
-			
-			if (socket != null)
-				socket.close();
-		}
+        DataOutputStream dataOutputStream = null;
+        BufferedReader bufferedReader = null;
 
-		/*
-		 * if is OK then not infected, else, infected...
-		 */
-		if (!res.trim().equals("stream: OK")) {
-			result = 1;
-			addAspect();
-		}
+        String res = null;
+        try {
+            if (logger.isDebugEnabled()) {
+                logger.debug(getClass().getName() + "Send zINSTREAM");
+            }
 
-		return result;
-	}
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeBytes("zINSTREAM\0");
 
-	/*
-	 * Re-scanning
-	 * 
-	 * @see com.fegorsoft.alfresco.security.antivirus.VirusScanMode#rescan()
-	 */
-	@Override
-	public int rescan() throws IOException {
-		return scan();
-	}
+            if (logger.isDebugEnabled()) {
+                logger.debug(getClass().getName() + "Send stream for  " + data.available() + " bytes");
+            }
 
-	/*
-	 * Report
-	 * 
-	 * @see com.fegorsoft.alfresco.security.antivirus.VirusScanMode#report()
-	 */
-	@Override
-	public int report() throws IOException {
-		int result = 0;
-		return result;
-	}
+            while ((bytesRead = data.read(buffer)) != -1) {
+                dataOutputStream.writeInt(bytesRead);
+                dataOutputStream.write(buffer, 0, bytesRead);
+            }
 
-	/**
-	 * Add aspect Scaned From ClamAV is not assigned
-	 */
-	private void addAspect() {
-		
-		if (logger.isDebugEnabled()) {
-			logger.debug(getClass().getName() + "Adding aspect if not exist");
-		}
-		
-		if (!nodeService.hasAspect(nodeRef,
-				AlfviralModel.ASPECT_SCANNED_FROM_CLAMAV)) {
-			nodeService.addAspect(nodeRef,
-					AlfviralModel.ASPECT_SCANNED_FROM_CLAMAV, null);
-		}
+            dataOutputStream.writeInt(0);
+            dataOutputStream.write('\0');
+            dataOutputStream.flush();
 
-		if (logger.isInfoEnabled()) {
-			logger.info(getClass().getName()
-					+ ": [Aspect SCANNED_FROM_CLAMAV assigned for "
-					+ nodeRef.getId() + "]");
-		}
+            bufferedReader = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream(), StandardCharsets.US_ASCII));
 
-	}
+            res = bufferedReader.readLine();
 
-	/**
-	 * @param nodeService
-	 */
-	public void setNodeService(NodeService nodeService) {
-		this.nodeService = nodeService;
-	}
+            if (logger.isDebugEnabled()) {
+                logger.debug(getClass().getName() + "Result of scan is:  " + res);
+            }
 
-	/**
-	 * @param nodeRef
-	 */
-	public void setNodeRef(NodeRef nodeRef) {
-		this.nodeRef = nodeRef;
-	}
+        } finally {
+            if (bufferedReader != null)
+                bufferedReader.close();
 
-	/**
-	 * @param data
-	 */
-	public void setData(byte[] data) {
-		this.data = data;
-	}
+            if (dataOutputStream != null)
+                dataOutputStream.close();
 
-	/**
-	 * @param chunkSize
-	 */
-	public void setChunkSize(int chunkSize) {
-		this.chunkSize = chunkSize;
-	}
+            if (socket != null)
+                socket.close();
 
-	/**
-	 * @param port
-	 */
-	public void setPort(int port) {
-		this.port = port;
-	}
+            if (data != null)
+                data.close();
+        }
 
-	/**
-	 * @param host
-	 */
-	public void setHost(String host) {
-		this.host = host;
-	}
+        /*
+         * if is OK then not infected, else, infected...
+         */
+        if (!res.trim().equals("stream: OK")) {
+            result = 1;
+            addAspect();
+        }
 
-	/**
-	 * @param timeout
-	 */
-	public void setTimeout(int timeout) {
-		this.timeout = timeout;
-	}
+        return result;
+    }
+
+    /*
+     * Re-scanning
+     *
+     * @see com.fegorsoft.alfresco.security.antivirus.VirusScanMode#rescan()
+     */
+    @Override
+    public int rescan() throws IOException {
+        return scan();
+    }
+
+    /*
+     * Report
+     *
+     * @see com.fegorsoft.alfresco.security.antivirus.VirusScanMode#report()
+     */
+    @Override
+    public int report() throws IOException {
+        int result = 0;
+        return result;
+    }
+
+    /**
+     * Add aspect Scaned From ClamAV is not assigned
+     */
+    private void addAspect() {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug(getClass().getName() + "Adding aspect if not exist");
+        }
+
+        if (!nodeService.hasAspect(nodeRef,
+                AlfviralModel.ASPECT_SCANNED_FROM_CLAMAV)) {
+            nodeService.addAspect(nodeRef,
+                    AlfviralModel.ASPECT_SCANNED_FROM_CLAMAV, null);
+        }
+
+        if (logger.isInfoEnabled()) {
+            logger.info(getClass().getName()
+                    + ": [Aspect SCANNED_FROM_CLAMAV assigned for "
+                    + nodeRef.getId() + "]");
+        }
+
+    }
+
+    /**
+     * @param nodeService
+     */
+    public void setNodeService(NodeService nodeService) {
+        this.nodeService = nodeService;
+    }
+
+    /**
+     * @param nodeRef
+     */
+    public void setNodeRef(NodeRef nodeRef) {
+        this.nodeRef = nodeRef;
+    }
+
+    /**
+     * @param dataStream
+     */
+    public void setData(InputStream dataStream) {
+        this.data = dataStream;
+    }
+
+    /**
+     * @param chunkSize
+     */
+    public void setChunkSize(int chunkSize) {
+        this.chunkSize = chunkSize;
+    }
+
+    /**
+     * @param port
+     */
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    /**
+     * @param host
+     */
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    /**
+     * @param timeout
+     */
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
 
 }
